7117110ea62b3cd182ab9271322b184a541dd51d
Ondicho <albert.ondicho@moringaschool.com>
2022-06-05 12:56:17 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName;
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase()
+}
+
+
+function setBestCustomer(){
+    var bestCustomer='not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer='bob'
+}
+
+
+const leastFavoriteCustomer='snoop dogg'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='dr dre'
+}
